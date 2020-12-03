e006b4eae21c4a4564c6e49910acab265436571c
nguyenjon <jonathannguyen2@my.unt.edu>
2020-12-03 17:05:13 -0600

    added function declarations

--- database/db_manager.py
+++ database/db_manager.py
@@ -16,11 +16,23 @@ class DatabaseManager:
 
             create_department_table = '''CREATE TABLE Department(
                                             Dept_ID INT NOT NULL UNIQUE, 
+                                            Name VARCHAR(255), 
+	                                        Location VARCHAR(255),
+
                                             PRIMARY KEY(Dept_ID))'''
 
             create_employee_table = '''CREATE TABLE Employee(
                                             Employee_ID INT NOT NULL UNIQUE, 
+                                            First_Name VARCHAR(255) NOT NULL, 
+                                            Last_Name VARCHAR(255), 
+                                            Date_of_Birth DATE NOT NULL,
+                                            Email VARCHAR(255),
+                                            Phone_Num VARCHAR(255),
+                                            Dept_ID VARCHAR(255), 
+                                            Type VARCHAR(255),
+                                            Status VARCHAR(255),
                                             Dept_ID INT, 
+
                                             PRIMARY KEY(Employee_ID), 
                                             FOREIGN KEY(Dept_ID) REFERENCES Department(Dept_ID))'''
 
@@ -29,6 +41,7 @@ class DatabaseManager:
                                             Type VARCHAR(255),
                                             Pay DECIMAL(10,2), 
                                             Employee_ID INT, 
+
                                             PRIMARY KEY(Payroll_ID), 
                                             FOREIGN KEY(Employee_ID) REFERENCES Employee(Employee_ID))''';
 
@@ -38,23 +51,43 @@ class DatabaseManager:
         
     #--------------------- Department operations-------------------------------------------------
 
+    def add_department(self):
+        #"INSERT INTO Department(Dept_ID) VALUES (" + i +")";
+        pass
 
+    def edit_department(self):
+        pass
 
+    def delete_department(self):
+        pass
 
     #--------------------- Employee operations-------------------------------------------------
 
+    def add_employee(self):
+        pass
 
+    def edit_employee(self):
+        pass
+
+    def delete_employee(self):
+        pass
 
 
     #--------------------- Payroll operations-------------------------------------------------
 
+    def add_payroll(self):
+        pass
+
+    def edit_payroll(self):
+        pass
+
+    def delete_payroll(self):
+        pass
 
 
 
 
 
-
-    
     # sqlStatements = "CREATE TABLE Payslip(Payslip_ID INT NOT NULL UNIQUE, Deductions DECIMAL(10,2) NOT NULL, Hours DECIMAL(5,2) NOT NULL, Payroll_ID INT, Net_Pay DECIMAL(10,2) NOT NULL, Gross_Pay DECIMAL(10,2) NOT NULL, Start_Date DATE NOT NULL NOT NULL, End_Date DATE NOT NULL NOT NULL, PRIMARY KEY(Payslip_ID), FOREIGN KEY(Payroll_ID) REFERENCES Payroll(Payroll_ID))";
 
     # sqlStatements = "CREATE TABLE Bonus(Bonus_ID INT NOT NULL UNIQUE, Deductions DECIMAL(10,2) NOT NULL, Amount DECIMAL(10,2) NOT NULL, Payroll_ID INT, Date DATE NOT NULL NOT NULL, PRIMARY KEY(Bonus_ID), FOREIGN KEY(Payroll_ID) REFERENCES Payroll(Payroll_ID))";
